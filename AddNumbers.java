2c6fcf0e9fcd1ba45f335db4b909abdda2ef8d59
SurendraBhimineni <surendra.bhimineni93@gmail.com>
2020-01-25 20:37:10 +0530

    oldfile

--- AddNumbers.java
+++ AddNumbers.java
@@ -2,7 +2,7 @@ public class AddNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
+      int num1 = 245, num2 = 15, sum;
       sum = num1 + num2;
 
       System.out.println("Sum of these numbers: "+sum);
